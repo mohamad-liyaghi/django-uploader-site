e53bf58e3c21c3aab11c739f14f1743348af0dd2
Ml06py <liaghimohamad69@gmail.com>
2022-04-02 16:09:49 +0430

    User model added

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    is_special = models.BooleanField(default=False)
+    limit = models.PositiveSmallIntegerField(default=10,blank=True,null=True)
