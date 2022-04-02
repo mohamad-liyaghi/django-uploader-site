4dde48ceda57d878e2bae4ce873a61970e0a3398
Ml06py <liaghimohamad69@gmail.com>
2022-04-02 15:45:06 +0430

    Apps added

--- uploader/settings.py
+++ uploader/settings.py
@@ -9,13 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os,sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -37,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.account.apps.AccountConfig',
+    'apps.file.apps.FileConfig'
 ]
 
 MIDDLEWARE = [
